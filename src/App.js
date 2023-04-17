91f7ba167facaca8eb41442f876669ad6b02b299
Rutik Gagare <rutikgagare091971@gmail.com>
2023-04-17 16:38:05 +0530

    adding firebase

--- src/App.js
+++ src/App.js
@@ -1,100 +1,13 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import Nav from './Components/Home/Nav';
 import ProductSection from './Components/Products/ProductSection';
 import Cart from './Components/Carts/Cart';
 import CartProvider from './store/CartProvider';
 
-const itemList = [
-  {
-    id: "id1",
-    name: "Airdopes 115",
-    category: "Airdopes",
-    price: 1099,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/115-main1_1875x.png?v=1655368638'
-  },
-  {
-    id: "id2",
-    name: "boAt Rockerz 410",
-    category: "Headphone",
-    price: 1599,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-410-black_1875x.png?v=1613457276'
-  },
-  {
-    id: "id3",
-    name: "boAt Rockerz 450 Pro",
-    category: "Headphone",
-    price: 1500,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-img3_600x.png?v=1616562632'
-  },
-  {
-    id: "id4",
-    name: "boAt Airdopes 161",
-    category: "Headphone",
-    price: 1099,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_black_fa1c6ec3-93b7-443e-ae82-d5eeb34258f8_600x.png?v=1641206192'
-  },
-  {
-    id: "id5",
-    name: "boAt Airdopes 381 ",
-    category: "Airdopes",
-    price: 1299,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/3c53a1e3-bffe-4c6d-b102-cc409c1bfe71_600x.png?v=1625045117'
-  },
-  {
-    id: "id6",
-    name: "boAt Bassheads 100",
-    category: "Earphone",
-    price: 399,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/bassheads-100-make-in-india-orange_1875x.png?v=1613743116'
-  },
-  {
-    id: "id7",
-    name: "boAt Airdopes 383 ",
-    category: "Airdopes",
-    price: 1899,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/airdopes-383-black_600x.png?v=1613457326'
-  },
-  {
-    id: "id8",
-    name: "boAt Rockerz 510",
-    category: "Headphone",
-    price: 1599,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/5d5377dc-d3dd-48ba-8ec9-2a1630146d8d_600x.png?v=1625045983'
-  },
-  {
-    id: "id9",
-    name: "boAt Rockerz 260",
-    category: "Bluetooth",
-    price: 1299,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main3_e0d541c5-72de-4f54-a19b-1c324082742a_600x.png?v=1654195093'
-  },
-  {
-    id: "id10",
-    name: "boAt BassHeads 220",
-    category: "Earphone",
-    price: 699,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Orange_05fdf488-bd1f-437e-8d13-dc428e957628_2188x.png?v=1660654083'
-  },
-  {
-    id: "id11",
-    name: "boAt Airdopes 131",
-    category: "AirDopes",
-    price: 1499,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Red_d8ee4785-21ae-4fee-943e-d5c571131ae8_1300x.png?v=1660654082'
-  },
-  {
-    id: "id12",
-    name: "Boat BassHeads 100",
-    category: "EarPhone",
-    price: 399,
-    url: 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/c3d0fbbd-9479-4351-bf04-91136da2ae91_600x.png?v=1656750183'
-  }
-];
-
 function App() {
   const [showCart, setShowCart] = useState(false);
-
+  
   const showCartHandler = () => {
     setShowCart(true);
   }
@@ -105,9 +18,9 @@ function App() {
 
   return (
     <CartProvider className="App">
-        <Nav showCart={showCartHandler}></Nav>
-        <ProductSection items={itemList}></ProductSection>
-        {showCart && <Cart onClose={closeCartHandler}></Cart>}
+      <Nav showCart={showCartHandler}></Nav>
+      <ProductSection></ProductSection>
+      {showCart && <Cart onClose={closeCartHandler}></Cart>}
     </CartProvider>
 
   );
--- src/Components/Products/ProductSection.js
+++ src/Components/Products/ProductSection.js
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Product from './Product';
 import classes from './ProductSection.module.css';
 
-function productSection(props) {
+function ProductSection(props) {
+  const [itemList, setItemList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchDataHandler() {
+      setIsLoading(true);
+
+      const response = await fetch('https://e-commerce-2b1c2-default-rtdb.firebaseio.com/products.json');
+      const data = await response.json();
+
+      console.log(data);
+
+      let loadedData = [];
+
+      for (const item in data) {
+        loadedData.push(
+          {
+            id: item,
+            name: data[item].name,
+            category: data[item].category,
+            price: data[item].price,
+            url: data[item].url
+          });
+      };
+
+      setItemList(loadedData);
+      setIsLoading(false);
+    }
+
+    fetchDataHandler();
+  }, []);
+
+  if (isLoading) {
+    return (<p className={classes.loading}>Loading......</p>);
+  }
+
   return (
     <div className={classes.productSection}>
       <div className={classes.heading}>
         <h2>Product Section</h2>
       </div>
-      
       <div className={classes.productSection_content}>
-      {
-        props.items.map((item) => {
-          return <Product
-            key={item.id}
-            id={item.id}
-            name = {item.name}
-            category = {item.category}
-            price = {item.price}
-            url = {item.url}
-          ></Product>
-        })
-      }
+        {
+          itemList.map((item) => {
+            return <Product
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              category={item.category}
+              price={item.price}
+              url={item.url}
+            ></Product>
+          })
+        }
       </div>
-     
     </div>
   )
 }
 
-export default productSection
+export default ProductSection
